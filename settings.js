90ebead6fe2e873c44034dc95ab03594f9416680
Lucky Tech Hub <www.luckytechhub@gmail.com>
2025-06-19 13:58:01 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Lucky Tech Hub Bot',
   author: '‎',
   botName: "Lucky Tech Hub Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '256789966218', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Lucky218', // Your name
+  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
