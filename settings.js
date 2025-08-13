ef9f6595b9f866b20c5535f5470e277427058274
hashansampath377-coder <hashansampath377@gmail.com>
2025-08-13 09:37:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Lucky Tech Hub Bot',
+  packname: 'Panda Bot',
   author: '‎',
-  botName: "Lucky Tech Hub Bot",
-  botOwner: 'Lucky218', // Your name
-  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Panda Bot",
+  botOwner: 'Hashan', // Your name
+  ownerNumber: '94782543893', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.8",
 };
